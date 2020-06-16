fdfa7bb1232fe72ae4abffb50cddfbd1c14deb83
anson <ansonchongwus44@gmail.com>
2020-06-16 13:59:39 -0700

    clean up

--- popup.js
+++ popup.js
@@ -16,7 +16,7 @@ function processInput(input) {
   })
 }
 
-function sendMessage(request) {
+function sendMessage (request) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, request, function (response) {
       console.log(response.status);
