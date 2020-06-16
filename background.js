cbc80ff5a36ed07d670bf262204c7f9f4f08b15d
anson <ansonchongwus44@gmail.com>
2020-06-16 15:28:38 -0700

    style code

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 chrome.runtime.onInstalled.addListener(function () {
-  console.log("Installed succesffully")
+  console.log("Installed succesffully");
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'www.interview-db.com' },
-      })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "www.interview-db.com" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
   });
 });
--- popup.js
+++ popup.js
@@ -5,18 +5,18 @@ const oneClickButton = document.getElementById("oneClick");
 const removeAllButton = document.getElementById("removeAll");
 
 function processInput(input) {
-  return input.map( (el) => {
+  return input.map((el) => {
     let pointer = el.length - 1;
-   
+
     while (pointer >= 0 && el[pointer] === " ") {
       pointer -= 1;
     }
 
     return el.slice(0, pointer + 1);
-  })
+  });
 }
 
-function sendMessage (request) {
+function sendMessage(request) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, request, function (response) {
       console.log(response.status);
@@ -37,7 +37,7 @@ select.onchange = function (event) {
     let label = document.createElement("label");
     label.innerText = types[i];
     if (types[i] === "category") label.innerText += " default: clearbit";
-    let input = document.createElement("textarea")
+    let input = document.createElement("textarea");
     input.id = "input-" + types[i];
     inputs.push(input);
     label.append(input);
@@ -47,11 +47,13 @@ select.onchange = function (event) {
   let button = document.createElement("button");
   button.innerText = "Submit";
   container.append(button);
-  
+
   button.onclick = function (e) {
     console.log(inputs);
-    let processedInputs = inputs.map( input => processInput(input.value.split("\n")));
-    let request; 
+    let processedInputs = inputs.map((input) =>
+      processInput(input.value.split("\n"))
+    );
+    let request;
 
     if (event.target.value === "networking") {
       if (processedInputs[2].length === 0) {
@@ -62,30 +64,30 @@ select.onchange = function (event) {
         type: "networking",
         contacts: processedInputs[0],
         companies: processedInputs[1],
-        categories: processedInputs[2]
-      }
+        categories: processedInputs[2],
+      };
     } else if (event.target.value === "applications") {
       request = {
         type: "applications",
         companies: processedInputs[0],
         titles: processedInputs[1],
-        jobBoards: processedInputs[2]
-      }
+        jobBoards: processedInputs[2],
+      };
     }
 
     sendMessage(request);
-  }
-}
+  };
+};
 
 resetButton.onclick = function (e) {
   container.innerHTML = "";
   select.selectedIndex = 0;
-}
+};
 
 oneClickButton.onclick = function (e) {
-  sendMessage({ type: "oneClick" })
-}
+  sendMessage({ type: "oneClick" });
+};
 
 removeAllButton.onclick = function (e) {
-  sendMessage({ type: "removeAll" })
-}
+  sendMessage({ type: "removeAll" });
+};
